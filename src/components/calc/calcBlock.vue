e6c5727a329322edf0e066c1283e72be01cc88ae
Gleb <gleb4ist@gmail.com>
2020-05-21 15:23:57 +0300

    закончил перенос

---
+++ src/components/calc/calcBlock.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <a id="calculate"></a>
+    <section class="main_section main_section__calculate">
+      <div class="content-wrapper">
+        <!-- Выбор формы -->
+        <input
+          type="radio"
+          checked
+          name="form-type"
+          value="calc-price"
+          id="calc-price"
+          class="novisible"
+        />
+        <input
+          type="radio"
+          name="form-type"
+          value="search-price"
+          id="search-price"
+          class="novisible"
+        />
+
+        <div class="form-type-setter">
+          <div class="form-type-setter_btn" id="analyze-init">
+            <label for="calc-price">Рассчитаем стоимость<br />квартиры</label>
+          </div>
+          <div class="form-type-setter_btn" id="search-init">
+            <label for="search-price">Найти выгодное<br />предложение</label>
+          </div>
+        </div>
+        <!-- / Выбор формы -->
+
+        <calc-content></calc-content>
+
+        <div class="calculate-form_result" id="results" style="display: none;">
+          <div class="calculate-form_result_item">
+            <div class="calculate-form_result_item_title">
+              Рекомендованная цена
+            </div>
+            <div
+              class="calculate-form_result_item_value"
+              id="result_price"
+            ></div>
+          </div>
+          <div class="calculate-form_result_item">
+            <div class="calculate-form_result_item_title">
+              Среднее время продажи
+            </div>
+            <div
+              class="calculate-form_result_item_value"
+              id="result_duration"
+            ></div>
+          </div>
+
+          <div id="chartdiv" style="height: 400px; width: 100%;"></div>
+
+          <div class="calculate-form_error" id="empty-msg">
+            <span>Недостаточно данных</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import calcContent from "./calcContent";
+export default {
+  components: {
+    calcContent,
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
